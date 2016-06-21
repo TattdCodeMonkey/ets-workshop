0b11e11c0c8394e4fe365588cb8e97fbc7f7e031
Rodney Norris <Rodney.Norris@gmail.com>
2016-06-20 20:50:03 -0500

    agent -> ets

--- example-1-wordcount/lib/wordcount/state.ex
+++ example-1-wordcount/lib/wordcount/state.ex
@@ -1,27 +1,31 @@
 defmodule WordCount.State do
-  defp name, do: __MODULE__
-  
-  def init, do: Agent.start_link(&HashDict.new/0, name: name)
+  @default_data []
 
-  def get, do: Agent.get(name, &(&1))
+  defp name, do: __MODULE__
+
+  def init do
+    case :ets.info(name) do
+      :undefined ->
+        name = :ets.new(name, [:set, :named_table, :public])
+        true = :ets.insert(name, @default_data)
+        {:ok, name}
+      _ -> {:ok, name}
+    end
+  end
+
+  def get, do: :ets.match_object(name, {:"$1", :"$2"})
 
   def increment_word(word) do
-    Agent.update(
-      name,
-      fn val ->
-        Dict.update(val, word, 1, &(&1 + 1))
-      end
-    )
+    :ets.update_counter(name, word, 1, {word, 0})
   end
 
   def clear_words do
-    Agent.update(name, fn _ -> HashDict.new end)
+    true = :ets.delete_all_objects(name)
+    :ok
   end
 
   def sort_desc do
     current = get
-    sorted = Enum.sort(current, fn {_, v1}, {_, v2} -> v1 > v2 end)
-    Agent.update(name, fn _ -> sorted end)
-    sorted
+    Enum.sort(current, fn {_, v1}, {_, v2} -> v1 > v2 end)
   end
 end
